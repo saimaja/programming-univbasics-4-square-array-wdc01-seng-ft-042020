e367a7c78071d759326746ebd0199146765c7361
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-27 03:22:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
   # your code here
-  result = Array.new(array.size)
+  result = Array.new(array.length)
   i = 0
-  while i < numbers.size
+  while i < numbers.length
   result[i] = numbers[i] ** 2
-  i += 1
-end
+    i += 1
+  end
  result
 end
